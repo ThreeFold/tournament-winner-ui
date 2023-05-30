3b32bf319b43c792761f435c171cc2928cd5b0fd
nmcnew <nmcnew@proton.me>
2023-05-29 20:22:57 -0600

    Working on stuff

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TournamentWinner.Api.Data;
+using TournamentWinner.Api.Models;
+
+namespace TournamentWinner.Api.Controllers;
+
+[ApiController]
+public class AccountController {
+
+}
